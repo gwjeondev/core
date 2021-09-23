32fe55db0402b0b0949dc1bcfee3d18a8f29de27
won-developer <gw4609@gmail.com>
2021-09-23 15:20:49 +0900

    init

---
+++ src/main/java/hello/core/CoreApplication.java
@@ -0,0 +1,16 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Calendar;
+
+@SpringBootApplication
+public class CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApplication.class, args);
+    }
+
+}
---
+++ src/main/java/hello/core/service/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package hello.core.service;
+
+import hello.core.member.Member;
+
+public class MemberServiceImpl implements MemberService {
+    @Override
+    public void join(Member member) {
+
+    }
+
+    @Override
+    public void findMember(Long memberId) {
+
+    }
+}

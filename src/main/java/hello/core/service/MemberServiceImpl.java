7fdaee825a0e590e83fe5bbbf6767def234f1317
won-developer <gw4609@gmail.com>
2021-09-23 15:27:25 +0900

    interface convention comments

--- src/main/java/hello/core/service/MemberServiceImpl.java
+++ src/main/java/hello/core/service/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.service;
 
 import hello.core.member.Member;
 
+// 관례..같은 것으로 인터페이스를 구현하는 구현체가 단일 구현체라면 뒤에 Impl를 쓴다.
 public class MemberServiceImpl implements MemberService {
     @Override
     public void join(Member member) {

a449dfb795fef296b6f2c3b7ea40c735082a1c95
won-developer <gw4609@gmail.com>
2021-09-25 21:44:44 +0900

    스프링 컨테이너와 스프링 빈 - 컨테이너에 등록된 모든 빈 조회

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -10,10 +10,22 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class MemberApp {
     public static void main(String[] args) {
-        // spring container 등록
+/*      ================= spring container 등록 =======================
+        - ApplicationContext 를 스프링 컨테이너라 한다.
+        - ApplicationContext 는 인터페이스이다.
+        - 스프링 컨테이너는 XML을 기반으로 만들 수 있고, 애노테이션 기반의 자바 설정 클래스로 만들 수 있다.
+        - 직전에 AppConfig 를 사용했던 방식이 애노테이션 기반의 자바 설정 클래스로 스프링 컨테이너를 만든 것이다.
+        - 자바 설정 클래스를 기반으로 스프링 컨테이너( ApplicationContext )를 만들어보자.
+        - new AnnotationConfigApplicationContext(AppConfig.class);
+        - 이 클래스는 ApplicationContext 인터페이스의 구현체이다.
+        - 스프링 컨테이너를 생성할 때는 구성 정보를 지정해주어야 한다.
+        - 여기서는 AppConfig.class 를 구성 정보로 지정했다.
+        ==============================================================*/
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        // get bean
+/*      ======================= get bean =============================
+        bean의 이름은 AppConfig 클래스의 method명이며, 타입으로는 MemberService, OrderService 인터페이스 임.
+        ==============================================================*/
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 

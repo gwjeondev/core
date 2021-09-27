f6c6d142d024b1e06c5809e55f4f00c56934555d
won-developer <gw4609@gmail.com>
2021-09-27 21:01:31 +0900

    싱글톤 컨테이너 - 싱글톤 패턴

---
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -0,0 +1,15 @@
+package hello.core.singleton;
+
+public class SingletonService {
+    private static SingletonService singletonService  = new SingletonService();
+
+    public static SingletonService getInstance() {
+        return singletonService;
+    }
+
+    private SingletonService() {}
+
+    public void logic() {
+        System.out.println("싱글톤 객체 로직 호출");
+    }
+}
---
+++ src/test/java/hello/core/singleton/SingletonServiceTest.java
@@ -0,0 +1,17 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonServiceTest {
+    @Test
+    @DisplayName("싱글톤패턴이 적용 되었는지?")
+    void singletonCheck() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+    }
+}

976d9358bdbddf27143dee387356a9dd7988130d
won-developer <gw4609@gmail.com>
2021-10-01 22:17:18 +0900

    의존관계 자동 주입: 옵션 처리

--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -19,18 +19,23 @@ public class AutowiredTest {
 
     static class TestBean {
         @Autowired(required = false)
+/*        Parameter Member는 스프링이 관리하는 빈이 아님으로 Autowired required를 false로 지정함.
+        default는 true이며 반드시 Parameter 타입이 Bean으로 등록되있어야 함, false일 경우 선택적으로 주입함으로
+        자동 주입 할 대상이 없으면(Bean) 호출하지 않음 */
         public void setNoBean(Member member) {
-            System.out.println("TestBean.setNoBean");
+            System.out.println("member = " + member);
         }
 
         @Autowired
+        //자동주입할 대상(Bean)이 없으면 null이 입력
         public void setNoBean1(@Nullable Member member) {
-            System.out.println("TestBean.setNoBean1");
+            System.out.println("member = " + member);
         }
 
         @Autowired
+        //자동주입할 대상이(Bean) 없으면 Optional.empty가 입력됨.
         public void setNoBean2(Optional<Member> member) {
-            System.out.println("TestBean.setNoBean2");
+            System.out.println("member = " + member);
         }
     }
 

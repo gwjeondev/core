00187333f42fdf62aa0917ae77a28d5a200cdbe0
won-developer <gw4609@gmail.com>
2021-10-02 22:32:35 +0900

    빈 생명주기 콜백: 애노테이션 @PostConstruct, @PreDestory

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 //public class NetworkClient implements InitializingBean, DisposableBean {
 public class NetworkClient {
     private String url;
@@ -27,12 +30,14 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init()  { // 의존관계 주입이 끝난후 콜백
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() { // 빈이 종료전 콜백
         System.out.println("NetworkClient.destroy");
         disconnect();
